e76de69e047448a593d2e1b61c6e3116b2acad21
LOYI4o2ropo[ <e.maykov@r-top.ru>
2023-01-19 18:04:11 +0300

    added of page news and partners

--- dist/js/index.js
+++ dist/js/index.js
@@ -2,29 +2,92 @@
 "use strict";
 
 document.addEventListener("DOMContentLoaded", function () {
-  var swiperNews1 = new Swiper(".news__slider-first", {
-    slidesPerView: 3,
-    spaceBetween: 30,
+  var swiperNewsFirst = new Swiper(".news__slider-first", {
+    slidesPerView: 4,
+    spaceBetween: 20,
     navigation: {
-      nextEl: ".news__btn-next-1",
-      prevEl: ".news__btn-prev-1"
+      nextEl: ".news__next-first",
+      prevEl: ".news__prev-first"
     },
     clickable: true,
     pagination: {
-      el: ".news-pagination-1"
+      el: ".news__pagination-first"
     },
     breakpoints: {
       320: {
-        slidesPerView: 1,
+        slidesPerView: 1.5,
         spaceBetween: 10
       },
-      400: {
+      525: {
         slidesPerView: 2,
+        spaceBetween: 10
+      },
+      767: {
+        slidesPerView: 3,
         spaceBetween: 20
       },
-      770: {
-        slidesPerView: 3,
+      990: {
+        slidesPerView: 4,
+        spaceBetween: 20
+      },
+      1330: {
+        slidesPerView: 4,
         spaceBetween: 30
+      }
+    }
+  });
+  var swiperNewsSecond = new Swiper(".news__slider-second", {
+    slidesPerView: 4,
+    spaceBetween: 20,
+    navigation: {
+      nextEl: ".news__next-second",
+      prevEl: ".news__prev-second"
+    },
+    clickable: true,
+    pagination: {
+      el: ".news__pagination-second"
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1.5,
+        spaceBetween: 10
+      },
+      525: {
+        slidesPerView: 2,
+        spaceBetween: 10
+      },
+      767: {
+        slidesPerView: 3,
+        spaceBetween: 20
+      },
+      990: {
+        slidesPerView: 4,
+        spaceBetween: 20
+      },
+      1330: {
+        slidesPerView: 4,
+        spaceBetween: 30
+      }
+    }
+  });
+  var swiperPartners = new Swiper(".partners__slider", {
+    slidesPerView: 6,
+    spaceBetween: 50,
+    navigation: {
+      nextEl: ".partners__next",
+      prevEl: ".partners__prev"
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 3.5,
+        spaceBetween: 10
+      },
+      767: {
+        spaceBetween: 37
+      },
+      1330: {
+        slidesPerView: 6,
+        spaceBetween: 60
       }
     }
   });
