0fec6f502ae8802b973185d7a8330065b77ecfd2
LOYI4o2ropo[ <e.maykov@r-top.ru>
2023-01-23 17:58:27 +0300

    added footer and mobile menu

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,28 +1,147 @@
 "use strict";
 
 document.addEventListener("DOMContentLoaded", function () {
-  var locationButton = document.querySelector(".location__button");
-  var locationItem = document.querySelectorAll(".location__item");
-  var locationModal = document.querySelector(".location");
-  var html = document.querySelector('html');
-  var body = document.querySelector('body');
-  var header = document.querySelector('header');
-  locationButton.addEventListener('click', function () {
-    locationModal.classList.add("visible");
-
-    if (locationModal.classList.contains("visible")) {
-      html.classList.add("no-scroll");
-      body.classList.add("mr-scroll");
-      header.classList.add("pr-scroll");
-    }
+  // --------------- Mobile menu ---------------
+  var burger = document.querySelector('.header__burger');
+  var menu = document.querySelector('.mobile-menu');
+  var close = document.querySelector('.mobile-menu__close');
+  burger.addEventListener("click", function () {
+    menu.classList.add('active');
+    document.body.classList.add("no-scroll");
   });
-  locationItem.forEach(function (item) {
-    item.addEventListener('click', function () {
-      locationModal.classList.toggle("visible");
-      html.classList.remove("no-scroll");
-      body.classList.remove("mr-scroll");
-      header.classList.remove("pr-scroll");
+  close.addEventListener("click", function () {
+    menu.classList.remove('active');
+    document.body.classList.remove("no-scroll");
+  }); // Выбор опций/сортировка
+
+  var handleOption = function handleOption(el) {
+    el = el.target; // открытие списков
+
+    if (el.closest(".linkHasChild")) {
+      document.querySelectorAll(".linkHasChild").forEach(function (el) {
+        el.classList.remove("active");
+      });
+      el.closest(".linkHasChild").classList.add("active");
+    }
+  };
+
+  document.addEventListener("click", handleOption); // ---------------Address maps---------------
+
+  ymaps.ready(function () {
+    var myMap = new ymaps.Map("map-1", {
+      center: [56.304903, 43.992027],
+      zoom: 14
+    }, {
+      searchControlProvider: "yandex#search"
+    }),
+        // Создаём макет содержимого.
+    MyIconContentLayout = ymaps.templateLayoutFactory.createClass('<div style="background: #fff; display: flex; padding: 5px 10px; justify-content: center; align-items: center; border-radius: 10px; box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1); width:150px; height: 35px; color: #262728; font-weight: 700; letter-spacing: 0.02em; line-height: 140%; font-size: 14px;">$[properties.iconContent]</div>'),
+        myPlacemarkWithContent = new ymaps.Placemark([56.310483, 43.992027], {
+      iconContent: "ул. Костина, 6к1"
+    }, {
+      // Опции.
+      // Необходимо указать данный тип макета.
+      iconLayout: "default#imageWithContent",
+      // Своё изображение иконки метки.
+      iconImageHref: "../assets/img/location.png",
+      // Размеры метки.
+      iconImageSize: [42, 60],
+      // Смещение левого верхнего угла иконки относительно
+      // её "ножки" (точки привязки).
+      iconImageOffset: [-24, -24],
+      // Смещение слоя с содержимым относительно слоя с картинкой.
+      iconContentOffset: [60, 10],
+      // Макет содержимого.
+      iconContentLayout: MyIconContentLayout
     });
+    myMap.geoObjects.add(myPlacemarkWithContent);
+  });
+  ymaps.ready(function () {
+    var myMap = new ymaps.Map("map-2", {
+      center: [56.304903, 43.992027],
+      zoom: 14
+    }, {
+      searchControlProvider: "yandex#search"
+    }),
+        // Создаём макет содержимого.
+    MyIconContentLayout = ymaps.templateLayoutFactory.createClass('<div style="padding: 5px 10px; background: #fff; display: flex; justify-content: center; align-items: center; border-radius: 10px; box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1); width:150px; height: 35px; color: #262728; font-weight: 700; letter-spacing: 0.02em; line-height: 140%; font-size: 14px;">$[properties.iconContent]</div>'),
+        myPlacemarkWithContent = new ymaps.Placemark([56.310483, 43.992027], {
+      iconContent: "ул. Коминтерна, 39 лит С7"
+    }, {
+      // Опции.
+      // Необходимо указать данный тип макета.
+      iconLayout: "default#imageWithContent",
+      // Своё изображение иконки метки.
+      iconImageHref: "../assets/img/location.png",
+      // Размеры метки.
+      iconImageSize: [42, 60],
+      // Смещение левого верхнего угла иконки относительно
+      // её "ножки" (точки привязки).
+      iconImageOffset: [-24, -24],
+      // Смещение слоя с содержимым относительно слоя с картинкой.
+      iconContentOffset: [60, 10],
+      // Макет содержимого.
+      iconContentLayout: MyIconContentLayout
+    });
+    myMap.geoObjects.add(myPlacemarkWithContent);
+  });
+  ymaps.ready(function () {
+    var myMap = new ymaps.Map("map-3", {
+      center: [56.304903, 43.992027],
+      zoom: 14
+    }, {
+      searchControlProvider: "yandex#search"
+    }),
+        // Создаём макет содержимого.
+    MyIconContentLayout = ymaps.templateLayoutFactory.createClass('<div style="padding: 5px 10px; background: #fff; display: flex; justify-content: center; align-items: center; border-radius: 10px; box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1); width:150px; height: 35px; color: #262728; font-weight: 700; letter-spacing: 0.02em; line-height: 140%; font-size: 14px;">$[properties.iconContent]</div>'),
+        myPlacemarkWithContent = new ymaps.Placemark([56.310483, 43.992027], {
+      iconContent: "ул. Ларина 27/4\n" + "(павильон 4/2)"
+    }, {
+      // Опции.
+      // Необходимо указать данный тип макета.
+      iconLayout: "default#imageWithContent",
+      // Своё изображение иконки метки.
+      iconImageHref: "../assets/img/location.png",
+      // Размеры метки.
+      iconImageSize: [42, 60],
+      // Смещение левого верхнего угла иконки относительно
+      // её "ножки" (точки привязки).
+      iconImageOffset: [-24, -24],
+      // Смещение слоя с содержимым относительно слоя с картинкой.
+      iconContentOffset: [60, 10],
+      // Макет содержимого.
+      iconContentLayout: MyIconContentLayout
+    });
+    myMap.geoObjects.add(myPlacemarkWithContent);
+  });
+  ymaps.ready(function () {
+    var myMap = new ymaps.Map("map-4", {
+      center: [56.304903, 43.992027],
+      zoom: 14
+    }, {
+      searchControlProvider: "yandex#search"
+    }),
+        // Создаём макет содержимого.
+    MyIconContentLayout = ymaps.templateLayoutFactory.createClass('<div style="padding: 5px 10px; background: #fff; display: flex; justify-content: center; align-items: center; border-radius: 10px; box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1); width:150px; height: 35px; color: #262728; font-weight: 700; letter-spacing: 0.02em; line-height: 140%; font-size: 14px;">$[properties.iconContent]</div>'),
+        myPlacemarkWithContent = new ymaps.Placemark([56.310483, 43.992027], {
+      iconContent: "ул. Новороссийская, 240 Ж"
+    }, {
+      // Опции.
+      // Необходимо указать данный тип макета.
+      iconLayout: "default#imageWithContent",
+      // Своё изображение иконки метки.
+      iconImageHref: "../assets/img/location.png",
+      // Размеры метки.
+      iconImageSize: [42, 60],
+      // Смещение левого верхнего угла иконки относительно
+      // её "ножки" (точки привязки).
+      iconImageOffset: [-24, -24],
+      // Смещение слоя с содержимым относительно слоя с картинкой.
+      iconContentOffset: [60, 10],
+      // Макет содержимого.
+      iconContentLayout: MyIconContentLayout
+    });
+    myMap.geoObjects.add(myPlacemarkWithContent);
   });
 });
 //# sourceMappingURL=main.js.map
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,27 +1,135 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-    const locationButton = document.querySelector(".location__button");
-    const locationItem = document.querySelectorAll(".location__item");
-    const locationModal = document.querySelector(".location");
-    const html = document.querySelector('html')
-    const body = document.querySelector('body')
-    const header = document.querySelector('header')
 
-    locationButton.addEventListener('click', () => {
-        locationModal.classList.add("visible")
 
-        if (locationModal.classList.contains("visible")){
-            html.classList.add("no-scroll")
-            body.classList.add("mr-scroll")
-            header.classList.add("pr-scroll")
-        }
-    })
-    locationItem.forEach(item => {
-        item.addEventListener('click', () => {
-            locationModal.classList.toggle("visible")
-            html.classList.remove("no-scroll")
-            body.classList.remove("mr-scroll")
-            header.classList.remove("pr-scroll")
-        })
-    })
+  // --------------- Mobile menu ---------------
+  const burger = document.querySelector('.header__burger')
+  const menu = document.querySelector('.mobile-menu')
+  const close = document.querySelector('.mobile-menu__close')
+
+  burger.addEventListener("click", () => {
+    menu.classList.add('active')
+    document.body.classList.add("no-scroll")
+  })
+  close.addEventListener("click", () => {
+    menu.classList.remove('active')
+    document.body.classList.remove("no-scroll")
+  })
+
+  // Выбор опций/сортировка
+  const handleOption = (el) => {
+    el = el.target;
+
+    // открытие списков
+    if (el.closest(".linkHasChild")) {
+      document.querySelectorAll(".linkHasChild").forEach(function (el) {
+        el.classList.remove("active")
+      })
+      el.closest(".linkHasChild").classList.add("active")
+    }
+  };
+  document.addEventListener("click", handleOption);
+
+
+
+  // ---------------Address maps---------------
+  ymaps.ready(function () {
+    var myMap = new ymaps.Map("map-1", {
+          center: [56.304903, 43.992027], zoom: 14,
+        },
+        {
+          searchControlProvider: "yandex#search",
+        }), // Создаём макет содержимого.
+      MyIconContentLayout = ymaps.templateLayoutFactory.createClass('<div style="background: #fff; display: flex; padding: 5px 10px; justify-content: center; align-items: center; border-radius: 10px; box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1); width:150px; height: 35px; color: #262728; font-weight: 700; letter-spacing: 0.02em; line-height: 140%; font-size: 14px;">$[properties.iconContent]</div>'),
+      myPlacemarkWithContent = new ymaps.Placemark([56.310483, 43.992027], {
+        iconContent: "ул. Костина, 6к1",
+      }, {
+        // Опции.
+        // Необходимо указать данный тип макета.
+        iconLayout: "default#imageWithContent", // Своё изображение иконки метки.
+        iconImageHref: "../assets/img/location.png", // Размеры метки.
+        iconImageSize: [42, 60], // Смещение левого верхнего угла иконки относительно
+        // её "ножки" (точки привязки).
+        iconImageOffset: [-24, -24], // Смещение слоя с содержимым относительно слоя с картинкой.
+        iconContentOffset: [60, 10], // Макет содержимого.
+        iconContentLayout: MyIconContentLayout,
+      });
+
+    myMap.geoObjects.add(myPlacemarkWithContent);
+  });
+  ymaps.ready(function () {
+    var myMap = new ymaps.Map("map-2", {
+          center: [56.304903, 43.992027], zoom: 14,
+        },
+        {
+          searchControlProvider: "yandex#search",
+        }), // Создаём макет содержимого.
+      MyIconContentLayout = ymaps.templateLayoutFactory.createClass('<div style="padding: 5px 10px; background: #fff; display: flex; justify-content: center; align-items: center; border-radius: 10px; box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1); width:150px; height: 35px; color: #262728; font-weight: 700; letter-spacing: 0.02em; line-height: 140%; font-size: 14px;">$[properties.iconContent]</div>'),
+      myPlacemarkWithContent = new ymaps.Placemark([56.310483, 43.992027], {
+        iconContent: "ул. Коминтерна, 39 лит С7",
+      }, {
+        // Опции.
+        // Необходимо указать данный тип макета.
+        iconLayout: "default#imageWithContent", // Своё изображение иконки метки.
+        iconImageHref: "../assets/img/location.png", // Размеры метки.
+        iconImageSize: [42, 60], // Смещение левого верхнего угла иконки относительно
+        // её "ножки" (точки привязки).
+        iconImageOffset: [-24, -24], // Смещение слоя с содержимым относительно слоя с картинкой.
+        iconContentOffset: [60, 10], // Макет содержимого.
+        iconContentLayout: MyIconContentLayout,
+      });
+
+    myMap.geoObjects.add(myPlacemarkWithContent);
+  });
+  ymaps.ready(function () {
+    var myMap = new ymaps.Map("map-3", {
+          center: [56.304903, 43.992027], zoom: 14,
+        },
+        {
+          searchControlProvider: "yandex#search",
+        }), // Создаём макет содержимого.
+      MyIconContentLayout = ymaps.templateLayoutFactory.createClass('<div style="padding: 5px 10px; background: #fff; display: flex; justify-content: center; align-items: center; border-radius: 10px; box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1); width:150px; height: 35px; color: #262728; font-weight: 700; letter-spacing: 0.02em; line-height: 140%; font-size: 14px;">$[properties.iconContent]</div>'),
+      myPlacemarkWithContent = new ymaps.Placemark([56.310483, 43.992027], {
+        iconContent: "ул. Ларина 27/4\n" +
+          "(павильон 4/2)",
+      }, {
+        // Опции.
+        // Необходимо указать данный тип макета.
+        iconLayout: "default#imageWithContent", // Своё изображение иконки метки.
+        iconImageHref: "../assets/img/location.png", // Размеры метки.
+        iconImageSize: [42, 60], // Смещение левого верхнего угла иконки относительно
+        // её "ножки" (точки привязки).
+        iconImageOffset: [-24, -24], // Смещение слоя с содержимым относительно слоя с картинкой.
+        iconContentOffset: [60, 10], // Макет содержимого.
+        iconContentLayout: MyIconContentLayout,
+      });
+
+    myMap.geoObjects.add(myPlacemarkWithContent);
+  });
+  ymaps.ready(function () {
+    var myMap = new ymaps.Map("map-4", {
+          center: [56.304903, 43.992027], zoom: 14,
+        },
+        {
+          searchControlProvider: "yandex#search",
+        }), // Создаём макет содержимого.
+      MyIconContentLayout = ymaps.templateLayoutFactory.createClass('<div style="padding: 5px 10px; background: #fff; display: flex; justify-content: center; align-items: center; border-radius: 10px; box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1); width:150px; height: 35px; color: #262728; font-weight: 700; letter-spacing: 0.02em; line-height: 140%; font-size: 14px;">$[properties.iconContent]</div>'),
+      myPlacemarkWithContent = new ymaps.Placemark([56.310483, 43.992027], {
+        iconContent: "ул. Новороссийская, 240 Ж",
+      }, {
+        // Опции.
+        // Необходимо указать данный тип макета.
+        iconLayout: "default#imageWithContent", // Своё изображение иконки метки.
+        iconImageHref: "../assets/img/location.png", // Размеры метки.
+        iconImageSize: [42, 60], // Смещение левого верхнего угла иконки относительно
+        // её "ножки" (точки привязки).
+        iconImageOffset: [-24, -24], // Смещение слоя с содержимым относительно слоя с картинкой.
+        iconContentOffset: [60, 10], // Макет содержимого.
+        iconContentLayout: MyIconContentLayout,
+      });
+
+    myMap.geoObjects.add(myPlacemarkWithContent);
+  });
+
 })
+
