bff8808c5d118274f139bce10de4b57ba18299f0
EugeneMaikov <you@example.com>
2023-01-29 15:03:10 +0300

    icons svg, refactoring

--- dist/js/main.js
+++ dist/js/main.js
@@ -26,7 +26,7 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   };
 
-  document.addEventListener("click", handleOption); // --------------- Выбор локации ---------------
+  document.addEventListener("click", handleOption); // --------------- Выбор города ---------------
 
   var filter = document.querySelector('.contacts__filter');
   var overlay = document.querySelector('.contacts__overlay');
@@ -45,7 +45,7 @@ document.addEventListener("DOMContentLoaded", function () {
       overlay.classList.remove('visible-filter');
       document.body.classList.remove("no-scroll");
     });
-  }); // --------------- Выбор города ---------------
+  }); // --------------- Mobile menu / Выбор города ---------------
 
   var locationBtn = document.querySelector('.location__button');
   var locationOverlay = document.querySelector('.location__overlay');
@@ -66,6 +66,163 @@ document.addEventListener("DOMContentLoaded", function () {
       locationModal.classList.remove('visible-location');
       locationOverlay.classList.remove('visible-location-overlay');
     });
-  });
+  }); // --------------- Маска ввода номера телефона ---------------
+
+  var phoneInputs = document.querySelectorAll("input[data-tel-input]");
+
+  var getInputsNumbersValue = function getInputsNumbersValue(input) {
+    return input.value.replace(/\D/g, "");
+  }; // Обработка ввода
+
+
+  var onPhoneInput = function onPhoneInput(e) {
+    var input = e.target;
+    var inputNumbersValue = getInputsNumbersValue(input);
+    var formattedInputValue = "";
+    var selectionStart = input.selectionStart;
+
+    if (!inputNumbersValue) {
+      return input.value = "";
+    }
+
+    if (input.value.length !== selectionStart) {
+      if (e.data && /\D/g.test(e.data)) {
+        input.value = inputNumbersValue;
+      }
+
+      return;
+    }
+
+    if (["7", "8", "9"].indexOf(inputNumbersValue[0]) > -1) {
+      // Российский номер
+      if (inputNumbersValue[0] === "9") {
+        inputNumbersValue = "7" + inputNumbersValue;
+      }
+
+      var firstSymbols = inputNumbersValue[0] === "8" ? "8" : "+7";
+      formattedInputValue = firstSymbols + " "; //Форматирование
+
+      if (inputNumbersValue.length > 1) {
+        formattedInputValue += "(" + inputNumbersValue.substring(1, 4);
+      }
+
+      if (inputNumbersValue.length >= 5) {
+        formattedInputValue += ") " + inputNumbersValue.substring(4, 7);
+      }
+
+      if (inputNumbersValue.length >= 8) {
+        formattedInputValue += "-" + inputNumbersValue.substring(7, 9);
+      }
+
+      if (inputNumbersValue.length >= 10) {
+        formattedInputValue += "-" + inputNumbersValue.substring(9, 11);
+      }
+    } else {
+      // Не российский номер
+      formattedInputValue = "+" + inputNumbersValue.substring(0, 16);
+    }
+
+    input.value = formattedInputValue;
+  }; //  Удаление первой цифры
+
+
+  var onPhoneKeyDown = function onPhoneKeyDown(e) {
+    var input = e.target;
+
+    if (e.keyCode === 8 && getInputsNumbersValue(input).length === 1) {
+      input.value = "";
+    }
+
+    if (e.keyCode === "-") {
+      input.value = "";
+    }
+  }; // Нельзя вставить букву из буфера обмена
+
+
+  var onPhonePaste = function onPhonePaste(e) {
+    var pasted = e.clipboardData || window.clipboardData;
+    var input = e.target;
+    var inputNumbersValue = getInputsNumbersValue(input);
+
+    if (pasted) {
+      var pastedText = pasted.getData("Text");
+
+      if (/\D/g.test(pastedText)) {
+        input.value = inputNumbersValue;
+      }
+    }
+  }; // Перебор полей с телефонами
+
+
+  for (var i = 0; i < phoneInputs.length; i++) {
+    var input = phoneInputs[i];
+    input.addEventListener("input", onPhoneInput);
+    input.addEventListener("keydown", onPhoneKeyDown);
+    input.addEventListener("paste", onPhonePaste);
+  } // ---------------Валидация---------------
+
+
+  var form = document.querySelector(".questions__form");
+  var formInputs = document.querySelectorAll(".form__input");
+  var inputEmail = document.querySelector(".js-input-email");
+  var inputPhone = document.querySelector(".js-input-phone");
+  var inputCheckbox = document.querySelector(".js-input-checkbox");
+
+  function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  }
+
+  function validateCountry(country) {
+    var re = new RegExp(".co$");
+    return re.test(String(country).toLowerCase());
+  } // Проходит автоматически, тк маска не пропускает некорректные данные
+
+
+  function validatePhone(phone) {
+    return test(String(phone));
+  } // Условия отправки
+
+
+  form.onsubmit = function () {
+    var emailVal = inputEmail.value;
+    var phoneVal = inputPhone.value;
+    var emptyInputs = Array.from(formInputs).filter(function (input) {
+      return input.value === "";
+    });
+    formInputs.forEach(function (input) {
+      if (input.value === "") {
+        input.classList.add("error");
+      } else {
+        input.classList.remove("error");
+      }
+    });
+
+    if (emptyInputs.length !== 0) {
+      console.log("inputs not filled");
+      return false;
+    }
+
+    if (!validateEmail(emailVal)) {
+      inputEmail.classList.add("error");
+      return false;
+    } else {
+      inputEmail.classList.remove("error");
+    }
+
+    if (validateCountry(emailVal)) {
+      inputEmail.classList.add("error");
+      return false;
+    } else {
+      inputEmail.classList.remove("error");
+    }
+
+    if (!validatePhone(phoneVal)) {
+      inputPhone.classList.add("error");
+      return false;
+    } else {
+      inputPhone.classList.remove("error");
+    }
+  };
 });
 //# sourceMappingURL=main.js.map
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -29,7 +29,7 @@ document.addEventListener("DOMContentLoaded", () => {
   };
   document.addEventListener("click", handleOption);
 
-  // --------------- Выбор локации ---------------
+  // --------------- Выбор города ---------------
   const filter = document.querySelector('.contacts__filter')
   const overlay = document.querySelector('.contacts__overlay')
   const contactsMobile = document.querySelectorAll('.contacts__mobile')
@@ -50,7 +50,7 @@ document.addEventListener("DOMContentLoaded", () => {
     })
   })
 
-  // --------------- Выбор города ---------------
+  // --------------- Mobile menu / Выбор города ---------------
   const locationBtn = document.querySelector('.location__button')
   const locationOverlay = document.querySelector('.location__overlay')
   const locationMobileBtn = document.querySelector('.mobile-menu__button')
@@ -73,6 +73,162 @@ document.addEventListener("DOMContentLoaded", () => {
       locationOverlay.classList.remove('visible-location-overlay')
     })
   })
+
+  // --------------- Маска ввода номера телефона ---------------
+  let phoneInputs = document.querySelectorAll("input[data-tel-input]");
+
+  let getInputsNumbersValue = function (input) {
+    return input.value.replace(/\D/g, "");
+  };
+
+// Обработка ввода
+  let onPhoneInput = function (e) {
+    let input = e.target;
+    let inputNumbersValue = getInputsNumbersValue(input);
+    let formattedInputValue = "";
+    let selectionStart = input.selectionStart;
+
+    if (!inputNumbersValue) {
+      return (input.value = "");
+    }
+
+    if (input.value.length !== selectionStart) {
+      if (e.data && /\D/g.test(e.data)) {
+        input.value = inputNumbersValue;
+      }
+      return;
+    }
+
+    if (["7", "8", "9"].indexOf(inputNumbersValue[0]) > -1) {
+      // Российский номер
+
+      if (inputNumbersValue[0] === "9") {
+        inputNumbersValue = "7" + inputNumbersValue;
+      }
+      let firstSymbols = inputNumbersValue[0] === "8" ? "8" : "+7";
+      formattedInputValue = firstSymbols + " ";
+
+      //Форматирование
+      if (inputNumbersValue.length > 1) {
+        formattedInputValue += "(" + inputNumbersValue.substring(1, 4);
+      }
+      if (inputNumbersValue.length >= 5) {
+        formattedInputValue += ") " + inputNumbersValue.substring(4, 7);
+      }
+      if (inputNumbersValue.length >= 8) {
+        formattedInputValue += "-" + inputNumbersValue.substring(7, 9);
+      }
+      if (inputNumbersValue.length >= 10) {
+        formattedInputValue += "-" + inputNumbersValue.substring(9, 11);
+      }
+    } else {
+      // Не российский номер
+      formattedInputValue = "+" + inputNumbersValue.substring(0, 16);
+    }
+
+    input.value = formattedInputValue;
+  };
+
+//  Удаление первой цифры
+  let onPhoneKeyDown = function (e) {
+    let input = e.target;
+    if (e.keyCode === 8 && getInputsNumbersValue(input).length === 1) {
+      input.value = "";
+    }
+    if (e.keyCode === "-") {
+      input.value = "";
+    }
+  };
+
+// Нельзя вставить букву из буфера обмена
+  let onPhonePaste = function (e) {
+    let pasted = e.clipboardData || window.clipboardData;
+    let input = e.target;
+    let inputNumbersValue = getInputsNumbersValue(input);
+
+    if (pasted) {
+      let pastedText = pasted.getData("Text");
+
+      if (/\D/g.test(pastedText)) {
+        input.value = inputNumbersValue;
+      }
+    }
+  };
+
+// Перебор полей с телефонами
+  for (let i = 0; i < phoneInputs.length; i++) {
+    let input = phoneInputs[i];
+    input.addEventListener("input", onPhoneInput);
+    input.addEventListener("keydown", onPhoneKeyDown);
+    input.addEventListener("paste", onPhonePaste);
+  }
+
+// ---------------Валидация---------------
+  let form = document.querySelector(".questions__form");
+  let formInputs = document.querySelectorAll(".form__input");
+  let inputEmail = document.querySelector(".js-input-email");
+  let inputPhone = document.querySelector(".js-input-phone");
+  let inputCheckbox = document.querySelector(".js-input-checkbox");
+
+  function validateEmail(email) {
+    let re =
+        /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  }
+
+  function validateCountry(country) {
+    let re = new RegExp(".co$");
+    return re.test(String(country).toLowerCase());
+  }
+
+// Проходит автоматически, тк маска не пропускает некорректные данные
+  function validatePhone(phone) {
+    return test(String(phone));
+  }
+
+// Условия отправки
+  form.onsubmit = function () {
+    let emailVal = inputEmail.value;
+    let phoneVal = inputPhone.value;
+    let emptyInputs = Array.from(formInputs).filter(
+        (input) => input.value === ""
+    );
+
+    formInputs.forEach(function (input) {
+      if (input.value === "") {
+        input.classList.add("error");
+      } else {
+        input.classList.remove("error");
+      }
+    });
+
+    if (emptyInputs.length !== 0) {
+      console.log("inputs not filled");
+      return false;
+    }
+
+    if (!validateEmail(emailVal)) {
+      inputEmail.classList.add("error");
+      return false;
+    } else {
+      inputEmail.classList.remove("error");
+    }
+
+    if (validateCountry(emailVal)) {
+      inputEmail.classList.add("error");
+      return false;
+    } else {
+      inputEmail.classList.remove("error");
+    }
+
+    if (!validatePhone(phoneVal)) {
+      inputPhone.classList.add("error");
+      return false;
+    } else {
+      inputPhone.classList.remove("error");
+    }
+  };
+
 
 
 })
