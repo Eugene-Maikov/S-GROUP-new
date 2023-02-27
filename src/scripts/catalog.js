273f9ea328073556a6630917bd562d6e55abf224
Eugene-Maikov <eugene-maikov2000@yandex.ru>
2023-02-27 14:41:57 +0300

    catalog cards under the form of a table

---
+++ src/scripts/catalog.js
@@ -0,0 +1,7 @@
+"use strict";
+document.addEventListener("DOMContentLoaded", () => {
+  // ---------------Tabs---------------
+  const displayTypeCatalog = new GraphTabs("displayTypeCatalog");
+});
+
+
