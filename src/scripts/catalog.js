df805e929d8027dc75452b81b34fe4ec61cdab27
Eugene-Maikov <eugene-maikov2000@yandex.ru>
2023-05-02 12:01:25 +0300

    fix: select in catalog-filter

--- src/scripts/catalog.js
+++ src/scripts/catalog.js
@@ -1,11 +1,49 @@
 "use strict";
 document.addEventListener("DOMContentLoaded", () => {
 
+  //Сортировка
+  const handleCatalogFilter = () => {
+    const filterBox = document.querySelector('.catalog-filter__action')
+    const filterItem = document.querySelectorAll('.catalog-filter__item')
+    const filterStatus = document.querySelector('.catalog-filter__status')
+    let filterStatusText = ''
+
+    // Открытие и закрытие списка
+    filterBox.addEventListener('click', () => {
+      filterBox.classList.toggle('active')
+    })
+
+    //Присваивание текста первому активному элементу
+    filterItem.forEach((item) => {
+      if (item.classList.contains('active')) {
+        filterStatusText = item.textContent
+      }
+    })
+    filterStatus.textContent = filterStatusText
+
+    // Переключение между активными элементами
+    const handleOption = (el) => {
+      el = el.target
+
+      if (el.closest(".catalog-filter__item")) {
+        filterItem.forEach(function (el) {
+          el.classList.remove("active")
+        })
+        el.closest(".catalog-filter__item").classList.add("active")
+        if (el.closest(".catalog-filter__item").classList.contains('active')) {
+          filterStatusText = el.textContent
+        }
+        filterStatus.textContent = filterStatusText
+      }
+    }
+    document.addEventListener("click", handleOption)
+  }
+
   // Открыть/закрыть элемент фильтра
   const handleGroupElements = () => {
     const filterGroupElements = document.querySelectorAll('.sidebar-filter__head')
 
-    if(filterGroupElements){
+    if (filterGroupElements) {
       filterGroupElements.forEach((el) => {
         el.addEventListener('click', () => {
           el.nextElementSibling.classList.toggle('active')
@@ -41,26 +79,31 @@ document.addEventListener("DOMContentLoaded", () => {
 
     let counter = 1
 
-    add.addEventListener('click', () => {
-      counter++
-      input.value = counter
-    })
-    remove.addEventListener('click', () => {
-      if(!counter <= 0) {
-        counter--
+    if (add) {
+      add.addEventListener('click', () => {
+        counter++
         input.value = counter
-      }
-    })
+      })
+    }
+    if (remove) {
+      remove.addEventListener('click', () => {
+        if (!counter <= 0) {
+          counter--
+          input.value = counter
+        }
+      })
+    }
+
   }
 
+  handleCatalogFilter()
   handleGroupElements()
   handleFilterModal()
   handleCounter()
 
-  Fancybox.bind('[data-fancybox="gallery"]', {
+  Fancybox.bind('["data-fancybox="gallery"]', {
     // Your custom options
   })
-
 
 
 })
